3365fd2cdb74b061079978b6f8b7c60df71afdf2
Menace 1-5 <dlghdus122@gmail.com>
2025-08-17 11:34:24 +0900

    refactor: CopyTo 메서드 도입 및 노드 저장 로직 단순화
    - 에디터 노드에 CopyTo 메서드 추가
        저장시, 이 메서드를 이용해서 런타임 노드에 속성을 복사
        FDialogueEdGraphNodeCreator에서 호출
        FDialogueEdGraphNodeCreator에서 구체적인 초기화 로직을 제거
    
    - 에디터 선택노드를 저장하는 과정에서 선택지를 평가하는 과정 제거
        런타임에 선택노드를 평가할 때 선택지를 동적으로 목록화 하도록 수정
    
    - GetPrevNodes, GetNextNode(s) 메서드의 템플릿화
        추후 인터페이스를 이용해서 각 노드의 연결 관계를 나타내게 수정할 필요가 있음
    
    - 일부 Setter 메서드를 #if WITH_EDITORONLY_DATA로 감쌌다.
        런타임시 데이터 오염 방지

---
+++ Source/DialogueSystemEditor/Private/Graph/Node/DialogueEdGraphCustomNode.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Graph/Node/DialogueEdGraphCustomNode.h"
+
+#include "Graph/Node/DialogueCustomNode.h"
+#include "Visitor/AbstractDialogueEdGraphVisitor.h"
+
+void UDialogueEdGraphCustomNode::Accept(FAbstractDialogueEdGraphVisitor* Visitor)
+{
+    Visitor->VisitCustomNode(this);
+}
+
+void UDialogueEdGraphCustomNode::CopyTo(const TObjectPtr<UDialogueCustomNode>& CustomNode) const
+{
+    CustomNode->SetWhenSelectThisNodeFunctionName(GetWhenSelectThisNodeFunctionName());
+    CustomNode->SetCanSelectThisNodeFunctionName(GetCanSelectThisNodeFunctionName());
+    auto Name = GetSelectionName();
+    CustomNode->SetSelectionName(Name);
+
+    CustomNode->SetDialogueSetting(GetDialogueSetting());
+}
+
+FText UDialogueEdGraphCustomNode::GetDefaultSelectionName() const
+{
+    return NSLOCTEXT("DialoguePlayer", "CustomNode", "커스텀 노드");
+}
---
+++ Source/DialogueSystemEditor/Public/Graph/Node/DialogueEdGraphCustomNode.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueEdGraphSceneNode.h"
+#include "DialogueEdGraphCustomNode.generated.h"
+
+class UDialogueCustomNode;
+class UDialogueSystemEvent;
+
+
+/// <summary>
+/// </summary>
+UCLASS()
+class DIALOGUESYSTEMEDITOR_API UDialogueEdGraphCustomNode : public UDialogueEdGraphSceneNode
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Accept(FAbstractDialogueEdGraphVisitor* Visitor) override;
+
+    virtual void CopyTo(const TObjectPtr<UDialogueCustomNode>& CustomNode) const;
+    virtual FText GetDefaultSelectionName() const override;
+
+protected:
+};
